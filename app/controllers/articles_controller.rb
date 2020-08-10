777ea4a97d93c55f18bc304ba7ec090a2d274247
Georjane <pzgeorjane@gmail.com>
2020-08-10 17:09:43 +0100

    Add edit and update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,26 @@ include ArticlesHelper
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
