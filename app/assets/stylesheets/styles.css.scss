3393ab3313e22a490d7020e2fe725e8c75cd02d9
Georjane <pzgeorjane@gmail.com>
2020-08-11 12:28:09 +0100

    Add a few gems

---
+++ app/assets/stylesheets/styles.css.scss
@@ -0,0 +1,36 @@
+$primary_color: #AAA;
+
+body {
+  background-color: $primary_color;
+  font: {
+    family: Verdana, Helvetica, Arial;
+    size: 14px;
+  }
+}
+
+a {
+  color: #0000FF;
+  img {
+    border: none;
+  }
+}
+
+.clear {
+  clear: both;
+  height: 0;
+  overflow: hidden;
+}
+
+#container {
+  width: 75%;
+  margin: 0 auto;
+  background: #fff;
+  padding: 20px 40px;
+  border: solid 1px black;
+  margin-top: 20px;
+}
+
+#content {
+  clear: both;
+  padding-top: 20px;
+}
