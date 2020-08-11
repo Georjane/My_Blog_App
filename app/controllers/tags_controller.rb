65e555e8b70ab232e801a68fc82147a61c1f44bf
Georjane <pzgeorjane@gmail.com>
2020-08-11 11:26:24 +0100

    Delete tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,11 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+    redirect_to tags_path
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :articles, through: :taggings
 
   def to_s
